ac7a5f235021d229c2c9b927467a6b5376d90332
Jones <lewiejones116@gmail.com>
2022-05-11 21:44:09 -0700

    rangfinder pwm and analog drivers return mm

--- libraries/AP_RangeFinder/AP_RangeFinder_PWM.h
+++ libraries/AP_RangeFinder/AP_RangeFinder_PWM.h
@@ -34,6 +34,8 @@ public:
     // update state
     void update(void) override;
 
+    bool supports_mm_precision() const override { return true; }
+
 protected:
 
     bool get_reading(float &reading_m);
