7b011d3b94c84e0da0fa5e47454a9c87ff683438
Jones <lewiejones116@gmail.com>
2022-05-12 16:23:49 -0700

    fix benewake and ground effect missions

--- libraries/AP_Mission/AP_Mission.cpp
+++ libraries/AP_Mission/AP_Mission.cpp
@@ -759,6 +759,7 @@ bool AP_Mission::stored_in_location(uint16_t id)
     case MAV_CMD_NAV_VTOL_TAKEOFF:
     case MAV_CMD_NAV_VTOL_LAND:
     case MAV_CMD_NAV_PAYLOAD_PLACE:
+    case 50:
         return true;
     default:
         return false;
@@ -913,6 +914,10 @@ MAV_MISSION_RESULT AP_Mission::mavlink_int_to_mission_cmd(const mavlink_mission_
 #endif
     }
     break;
+
+    case 50:
+        cmd.p1 = packet.param1;
+        break;
 
     case MAV_CMD_NAV_LOITER_UNLIM:                      // MAV ID: 17
         cmd.p1 = fabsf(packet.param3);                  // store radius as 16bit since no other params are competing for space
